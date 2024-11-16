7f13b5a076ef75abaac6064929dde9f9ff1f6c5c
Adam <classygopher@gmail.com>
2024-11-16 18:00:11 +0000

    update for buffer API changes

--- main.lua
+++ main.lua
@@ -66,9 +66,6 @@ bolt.onrender3d(function (event)
 
   local vertexcount = event:vertexcount()
   local animated = event:animated()
-  local images = {}
-  local imagecount = 0
-  local rgbabytesinmessage = 0
   local animations = {}
   local animationcount = 0
   local mm1,  mm2,  mm3,  mm4,
@@ -83,65 +80,53 @@ bolt.onrender3d(function (event)
   for i = 1, vertexcount do
     local atlasmeta = event:vertexmeta(i)
     local bone = event:vertexbone(i)
-    if images[atlasmeta] == nil then
-      local imgx, imgy, imgw, imgh = event:atlasxywh(atlasmeta)
-      local rgba = bolt.createbuffer(imgw * imgh * 4)
-      for j = 0, (imgh - 1) do
-        local pixelrow = event:texturedata(imgx, imgy + j, imgw * 4)
-        rgba:writestring(pixelrow, j * imgw * 4)
-      end
-      images[atlasmeta] = rgba
-      imagecount = imagecount + 1
-      rgbabytesinmessage = rgbabytesinmessage + (imgw * imgh * 4)
-    end
     if animated and animations[bone] == nil then
       animations[bone] = event:boneanimation(bone)
       animationcount = animationcount + 1
     end
   end
 
-  local messagesize = 16 + (16 * 8 * 2) + (vertexcount * 64) + (imagecount * 8) + rgbabytesinmessage + (16 * 8 * animationcount) + (8 * animationcount)
+  local messagesize = 16 + (16 * 8 * 2) + (vertexcount * 64) + (136 * animationcount)
   local message = bolt.createbuffer(messagesize)
   if event:animated() then
-    message:writeinteger(2, 0, 4)
+    message:setuint8(0, 2)
   else
-    message:writeinteger(1, 0, 4)
+    message:setuint8(0, 1)
   end
-  message:writeinteger(vertexcount, 4, 4)
-  message:writeinteger(imagecount, 8, 4)
-  message:writeinteger(animationcount, 12, 4)
-  message:writenumber(mm1, 16)
-  message:writenumber(mm2, 24)
-  message:writenumber(mm3, 32)
-  message:writenumber(mm4, 40)
-  message:writenumber(mm5, 48)
-  message:writenumber(mm6, 56)
-  message:writenumber(mm7, 64)
-  message:writenumber(mm8, 72)
-  message:writenumber(mm9, 80)
-  message:writenumber(mm10, 88)
-  message:writenumber(mm11, 96)
-  message:writenumber(mm12, 104)
-  message:writenumber(mm13, 112)
-  message:writenumber(mm14, 120)
-  message:writenumber(mm15, 128)
-  message:writenumber(mm16, 136)
-  message:writenumber(pm1, 144)
-  message:writenumber(pm2, 152)
-  message:writenumber(pm3, 160)
-  message:writenumber(pm4, 168)
-  message:writenumber(pm5, 176)
-  message:writenumber(pm6, 184)
-  message:writenumber(pm7, 192)
-  message:writenumber(pm8, 200)
-  message:writenumber(pm9, 208)
-  message:writenumber(pm10, 216)
-  message:writenumber(pm11, 224)
-  message:writenumber(pm12, 232)
-  message:writenumber(pm13, 240)
-  message:writenumber(pm14, 248)
-  message:writenumber(pm15, 256)
-  message:writenumber(pm16, 264)
+  message:setuint32(4, vertexcount)
+  message:setuint32(12, animationcount)
+  message:setfloat64(16, mm1)
+  message:setfloat64(24, mm2)
+  message:setfloat64(32, mm3)
+  message:setfloat64(40, mm4)
+  message:setfloat64(48, mm5)
+  message:setfloat64(56, mm6)
+  message:setfloat64(64, mm7)
+  message:setfloat64(72, mm8)
+  message:setfloat64(80, mm9)
+  message:setfloat64(88, mm10)
+  message:setfloat64(96, mm11)
+  message:setfloat64(104, mm12)
+  message:setfloat64(112, mm13)
+  message:setfloat64(120, mm14)
+  message:setfloat64(128, mm15)
+  message:setfloat64(136, mm16)
+  message:setfloat64(144, pm1)
+  message:setfloat64(152, pm2)
+  message:setfloat64(160, pm3)
+  message:setfloat64(168, pm4)
+  message:setfloat64(176, pm5)
+  message:setfloat64(184, pm6)
+  message:setfloat64(192, pm7)
+  message:setfloat64(200, pm8)
+  message:setfloat64(208, pm9)
+  message:setfloat64(216, pm10)
+  message:setfloat64(224, pm11)
+  message:setfloat64(232, pm12)
+  message:setfloat64(240, pm13)
+  message:setfloat64(248, pm14)
+  message:setfloat64(256, pm15)
+  message:setfloat64(264, pm16)
   local cursor = 272
 
   for i = 1, vertexcount do
@@ -150,51 +135,46 @@ bolt.onrender3d(function (event)
     local atlasmeta = event:vertexmeta(i)
     local u, v = event:vertexuv(i)
     local cr, cg, cb, ca = event:vertexcolour(i)
-    message:writeinteger(x, cursor, 2)
-    message:writeinteger(y, cursor + 2, 2)
-    message:writeinteger(z, cursor + 4, 2)
-    message:writeinteger(bone, cursor + 6, 2)
-    message:writenumber(u, cursor + 8)
-    message:writenumber(v, cursor + 16)
-    message:writeinteger(atlasmeta, cursor + 24, 4)
-    message:writeinteger(0, cursor + 28, 4) -- 4 unused bytes
-    message:writenumber(cr, cursor + 32)
-    message:writenumber(cg, cursor + 40)
-    message:writenumber(cb, cursor + 48)
-    message:writenumber(ca, cursor + 56)
+    local imgx, imgy, imgw, imgh = event:atlasxywh(atlasmeta)
+    message:setint16(cursor, x)
+    message:setint16(cursor + 2, y)
+    message:setint16(cursor + 4, z)
+    message:setint16(cursor + 6, bone)
+    message:setfloat64(cursor + 8, u)
+    message:setfloat64(cursor + 16, v)
+    message:setuint16(cursor + 24, imgw)
+    message:setuint16(cursor + 26, imgh)
+    message:setuint16(cursor + 28, imgw)
+    message:setuint16(cursor + 30, imgh)
+    message:setfloat64(cursor + 32, cr)
+    message:setfloat64(cursor + 40, cg)
+    message:setfloat64(cursor + 48, cb)
+    message:setfloat64(cursor + 56, ca)
     cursor = cursor + 64
-  end
-  for meta, rgba in pairs(images) do
-    local imgx, imgy, imgw, imgh = event:atlasxywh(meta)
-    message:writeinteger(meta, cursor, 4)
-    message:writeinteger(imgw, cursor + 4, 2)
-    message:writeinteger(imgh, cursor + 6, 2)
-    message:writebuffer(rgba, cursor + 8)
-    cursor = cursor + (imgw * imgh * 4) + 8
   end
   for bone, transform in pairs(animations) do
     local t1,  t2,  t3,  t4,
           t5,  t6,  t7,  t8,
           t9,  t10, t11, t12,
           t13, t14, t15, t16 = transform:get()
-    message:writeinteger(bone, cursor, 2)
-    message:writeinteger(0, cursor + 2, 6) -- 6 unused bytes
-    message:writenumber(t1, cursor + 8)
-    message:writenumber(t2, cursor + 16)
-    message:writenumber(t3, cursor + 24)
-    message:writenumber(t4, cursor + 32)
-    message:writenumber(t5, cursor + 40)
-    message:writenumber(t6, cursor + 48)
-    message:writenumber(t7, cursor + 56)
-    message:writenumber(t8, cursor + 64)
-    message:writenumber(t9, cursor + 72)
-    message:writenumber(t10, cursor + 80)
-    message:writenumber(t11, cursor + 88)
-    message:writenumber(t12, cursor + 96)
-    message:writenumber(t13, cursor + 104)
-    message:writenumber(t14, cursor + 112)
-    message:writenumber(t15, cursor + 120)
-    message:writenumber(t16, cursor + 128)
+    message:setuint16(cursor, bone)
+    -- 6 unused bytes
+    message:setfloat64(cursor + 8, t1)
+    message:setfloat64(cursor + 16, t2)
+    message:setfloat64(cursor + 24, t3)
+    message:setfloat64(cursor + 32, t4)
+    message:setfloat64(cursor + 40, t5)
+    message:setfloat64(cursor + 48, t6)
+    message:setfloat64(cursor + 56, t7)
+    message:setfloat64(cursor + 64, t8)
+    message:setfloat64(cursor + 72, t9)
+    message:setfloat64(cursor + 80, t10)
+    message:setfloat64(cursor + 88, t11)
+    message:setfloat64(cursor + 96, t12)
+    message:setfloat64(cursor + 104, t13)
+    message:setfloat64(cursor + 112, t14)
+    message:setfloat64(cursor + 120, t15)
+    message:setfloat64(cursor + 128, t16)
     cursor = cursor + 136
   end
   capturebrowser:sendmessage(message)
