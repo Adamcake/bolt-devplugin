d7ab63a8a628324ec9a5ff97da5a830de31487a5
Adam <classygopher@gmail.com>
2024-12-01 19:01:13 +0000

    rest of the item icon stuff

--- app/index.js
+++ app/index.js
@@ -122,6 +122,7 @@ let minimaptex = null;
 let minimapWidth;
 let minimapHeight;
 let minimapfb;
+let identityMatrix;
 
 const windoww = params.get('w');
 const windowh = params.get('h');
@@ -219,32 +220,14 @@ const redraw = () => {
                 gl.drawArrays(gl.TRIANGLES, 0, entity.vertexCount);
             }
 
-            if (entity.type === 'minimap') {
-                if (minimaptex === null) continue;
-                const x1 = entity.targetx;
-                const x2 = entity.targetx + entity.targetw;
-                const y1 = entity.targety;
-                const y2 = entity.targety + entity.targeth;
-                const u1 = entity.sourcex / minimapWidth;
-                const u2 = (entity.sourcex + entity.sourcew) / minimapWidth;
-                const v1 = entity.sourcey / minimapHeight;
-                const v2 = (entity.sourcey + entity.sourceh) / minimapHeight;
-                const square2d = gl.createBuffer();
-                gl.bindBuffer(gl.ARRAY_BUFFER, square2d);
-                gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-                    x1, y1, u1, v2, entity.sourcex, entity.sourcey, entity.sourcew, entity.sourceh, 1, 1, 1, 1, 0, 0, 0,
-                    x2, y1, u2, v2, entity.sourcex, entity.sourcey, entity.sourcew, entity.sourceh, 1, 1, 1, 1, 0, 0, 0,
-                    x1, y2, u1, v1, entity.sourcex, entity.sourcey, entity.sourcew, entity.sourceh, 1, 1, 1, 1, 0, 0, 0,
-                    x2, y1, u2, v2, entity.sourcex, entity.sourcey, entity.sourcew, entity.sourceh, 1, 1, 1, 1, 0, 0, 0,
-                    x2, y2, u2, v1, entity.sourcex, entity.sourcey, entity.sourcew, entity.sourceh, 1, 1, 1, 1, 0, 0, 0,
-                    x1, y2, u1, v1, entity.sourcex, entity.sourcey, entity.sourcew, entity.sourceh, 1, 1, 1, 1, 0, 0, 0,
-                ]), gl.STATIC_DRAW);
+            if (entity.type === 'itemicon') {
                 gl.disable(gl.DEPTH_TEST);
                 gl.useProgram(program2d);
                 gl.viewport(0, 0, windoww, windowh);
-                gl.uniform4fv(program2d_uAtlasWHScreenWH, [minimapWidth, minimapHeight, windoww, windowh]);
-                gl.bindTexture(gl.TEXTURE_2D, minimaptex);
+                gl.bindTexture(gl.TEXTURE_2D, entity.texture);
                 gl.uniform1i(program2d_uTex, 0); // because we activated TEXTURE0 earlier
+                gl.uniform4fv(program2d_uAtlasWHScreenWH, [64, 64, windoww, windowh]);
+                gl.bindBuffer(gl.ARRAY_BUFFER, entity.buffer);
                 for (let i = 0; i < maxAttribCount; i += 1) {
                     i < 4 ? gl.enableVertexAttribArray(i) : gl.disableVertexAttribArray(i);
                 }
@@ -253,7 +236,25 @@ const redraw = () => {
                 gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 60, 32);
                 gl.vertexAttribPointer(3, 3, gl.FLOAT, false, 60, 48);
                 gl.drawArrays(gl.TRIANGLES, 0, 6);
-                gl.deleteBuffer(square2d);
+            }
+
+            if (entity.type === 'minimap') {
+                if (minimaptex === null) continue;
+                gl.disable(gl.DEPTH_TEST);
+                gl.useProgram(program2d);
+                gl.viewport(0, 0, windoww, windowh);
+                gl.uniform4fv(program2d_uAtlasWHScreenWH, [minimapWidth, minimapHeight, windoww, windowh]);
+                gl.bindTexture(gl.TEXTURE_2D, minimaptex);
+                gl.uniform1i(program2d_uTex, 0); // because we activated TEXTURE0 earlier
+                gl.bindBuffer(gl.ARRAY_BUFFER, entity.buffer);
+                for (let i = 0; i < maxAttribCount; i += 1) {
+                    i < 4 ? gl.enableVertexAttribArray(i) : gl.disableVertexAttribArray(i);
+                }
+                gl.vertexAttribPointer(0, 4, gl.FLOAT, false, 60, 0);
+                gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 60, 16);
+                gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 60, 32);
+                gl.vertexAttribPointer(3, 3, gl.FLOAT, false, 60, 48);
+                gl.drawArrays(gl.TRIANGLES, 0, 6);
             }
         }
     } else {
@@ -410,13 +411,111 @@ const handleMessage = (message) => {
             break;
         }
         case 5: {
-            // todo
+            const step = 56;
             const targetx = arr.getInt16(4, true);
             const targety = arr.getInt16(6, true);
             const targetw = arr.getInt16(8, true);
             const targeth = arr.getInt16(10, true);
             const modelcount = arr.getUint32(12, true);
-            console.log(`icon at ${targetx},${targety} is ${targetw}x${targeth}, with ${modelcount} models`);
+            const texture = gl.createTexture();
+            const fb = gl.createFramebuffer();
+            gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, fb);
+            gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA8, 64, 64);
+            gl.framebufferTexture2D(gl.DRAW_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+            gl.disable(gl.SCISSOR_TEST);
+            gl.enable(gl.CULL_FACE);
+            gl.cullFace(gl.FRONT);
+            gl.viewport(0, 0, 64, 64);
+            gl.clearColor(0, 0, 0, 0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+
+            let models = Array(modelcount);
+            let cursor = 16;
+            for (let model = 0; model < modelcount; model += 1) {
+                const vertexcount = arr.getUint32(cursor, true);
+                let vertices = Array(vertexcount);
+                const bufferDataArray = new ArrayBuffer(vertexcount * step);
+                const bufferData = new DataView(bufferDataArray);
+                const viewMatrix = new Float32Array(16);
+                viewMatrix.set(new Float32Array(message, cursor + 4, 16));
+                const projMatrix = new Float32Array(16);
+                projMatrix.set(new Float32Array(message, cursor + 68, 16));
+                cursor += 132;
+                for (let vertex = 0; vertex < vertexcount; vertex += 1) {
+                    const v = {
+                        x: arr.getInt16(cursor, true),
+                        y: arr.getInt16(cursor + 2, true),
+                        z: arr.getInt16(cursor + 4, true),
+                        r: arr.getFloat32(cursor + 8, true),
+                        g: arr.getFloat32(cursor + 12, true),
+                        b: arr.getFloat32(cursor + 16, true),
+                        a: arr.getFloat32(cursor + 20, true),
+                    };
+                    const bufferDataOffset = vertex * step;
+                    bufferData.setFloat32(bufferDataOffset, v.x, true);
+                    bufferData.setFloat32(bufferDataOffset + 4, v.y, true);
+                    bufferData.setFloat32(bufferDataOffset + 8, v.z, true);
+                    bufferData.setFloat32(bufferDataOffset + 24, 0, true);
+                    bufferData.setFloat32(bufferDataOffset + 28, 0, true);
+                    bufferData.setFloat32(bufferDataOffset + 32, 64, true);
+                    bufferData.setFloat32(bufferDataOffset + 36, 64, true);
+                    bufferData.setFloat32(bufferDataOffset + 40, v.r, true);
+                    bufferData.setFloat32(bufferDataOffset + 44, v.g, true);
+                    bufferData.setFloat32(bufferDataOffset + 48, v.b, true);
+                    bufferData.setFloat32(bufferDataOffset + 52, v.a, true);
+                    vertices[vertex] = v;
+                    cursor += 24;
+                }
+                models[model] = {
+                    vertices,
+                    viewMatrix,
+                    projMatrix,
+                };
+
+                const buffer = gl.createBuffer();
+                gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+                gl.bufferData(gl.ARRAY_BUFFER, bufferData, gl.STATIC_DRAW);
+                gl.useProgram(program3d);
+                gl.uniformMatrix4fv(program3d_uModelMatrix, false, identityMatrix);
+                gl.uniformMatrix4fv(program3d_uViewMatrix, false, viewMatrix);
+                gl.uniformMatrix4fv(program3d_uProjMatrix, false, projMatrix);
+                gl.uniform2fv(program3d_uAtlasWH, [1, 1]);
+                gl.bindTexture(gl.TEXTURE_2D, whitePixelTex);
+                gl.uniform1i(program3d_uTex, 0); // because we activated TEXTURE0 earlier
+                for (let i = 0; i < maxAttribCount; i += 1) {
+                    i < 4 ? gl.enableVertexAttribArray(i) : gl.disableVertexAttribArray(i);
+                }
+                gl.vertexAttribPointer(0, 4, gl.FLOAT, false, step, 0);
+                gl.vertexAttribPointer(1, 2, gl.FLOAT, false, step, 16);
+                gl.vertexAttribPointer(2, 4, gl.FLOAT, false, step, 24);
+                gl.vertexAttribPointer(3, 4, gl.FLOAT, false, step, 40);
+                gl.drawArrays(gl.TRIANGLES, 0, vertexcount);
+                gl.deleteBuffer(buffer);
+            }
+            const x1 = targetx;
+            const x2 = targetx + targetw;
+            const y1 = targety;
+            const y2 = targety + targeth;
+            const buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+                x1, y1, 0, 1, 0, 0, 64, 64, 1, 1, 1, 1, 0, 0, 0,
+                x2, y1, 1, 1, 0, 0, 64, 64, 1, 1, 1, 1, 0, 0, 0,
+                x1, y2, 0, 0, 0, 0, 64, 64, 1, 1, 1, 1, 0, 0, 0,
+                x2, y1, 1, 1, 0, 0, 64, 64, 1, 1, 1, 1, 0, 0, 0,
+                x2, y2, 1, 0, 0, 0, 64, 64, 1, 1, 1, 1, 0, 0, 0,
+                x1, y2, 0, 0, 0, 0, 64, 64, 1, 1, 1, 1, 0, 0, 0,
+            ]), gl.STATIC_DRAW);
+            entities.push({
+                type: 'itemicon',
+                models,
+                texture,
+                buffer,
+            });
+            gl.bindFramebuffer(gl.DRAW_FRAMEBUFFER, null);
+            gl.deleteFramebuffer(fb);
+            gl.disable(gl.CULL_FACE);
             break;
         }
         case 6: {
@@ -506,7 +605,6 @@ const handleMessage = (message) => {
                 type: 'minimap2d',
                 textureId,
                 vbo,
-                vertexCount,
                 vertices,
             });
             receivedVertices += vertexCount;
@@ -514,16 +612,44 @@ const handleMessage = (message) => {
             break;
         }
         case 7: {
+            if (!minimaptex) break;
+            const sourcex = arr.getInt16(4, true);
+            const sourcey = arr.getInt16(6, true);
+            const sourcew = arr.getUint16(8, true);
+            const sourceh = arr.getUint16(10, true);
+            const targetx = arr.getInt16(12, true);
+            const targety = arr.getInt16(14, true);
+            const targetw = arr.getUint16(16, true);
+            const targeth = arr.getUint16(18, true);
+            const x1 = targetx;
+            const x2 = targetx + targetw;
+            const y1 = targety;
+            const y2 = targety + targeth;
+            const u1 = sourcex / minimapWidth;
+            const u2 = (sourcex + sourcew) / minimapWidth;
+            const v1 = sourcey / minimapHeight;
+            const v2 = (sourcey + sourceh) / minimapHeight;
+            const buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+                x1, y1, u1, v2, sourcex, sourcey, sourcew, sourceh, 1, 1, 1, 1, 0, 0, 0,
+                x2, y1, u2, v2, sourcex, sourcey, sourcew, sourceh, 1, 1, 1, 1, 0, 0, 0,
+                x1, y2, u1, v1, sourcex, sourcey, sourcew, sourceh, 1, 1, 1, 1, 0, 0, 0,
+                x2, y1, u2, v2, sourcex, sourcey, sourcew, sourceh, 1, 1, 1, 1, 0, 0, 0,
+                x2, y2, u2, v1, sourcex, sourcey, sourcew, sourceh, 1, 1, 1, 1, 0, 0, 0,
+                x1, y2, u1, v1, sourcex, sourcey, sourcew, sourceh, 1, 1, 1, 1, 0, 0, 0,
+            ]), gl.STATIC_DRAW);
             entities.push({
                 type: 'minimap',
-                sourcex: arr.getInt16(4, true),
-                sourcey: arr.getInt16(6, true),
-                sourcew: arr.getUint16(8, true),
-                sourceh: arr.getUint16(10, true),
-                targetx: arr.getInt16(12, true),
-                targety: arr.getInt16(14, true),
-                targetw: arr.getUint16(16, true),
-                targeth: arr.getUint16(18, true),
+                sourcex,
+                sourcey,
+                sourcew,
+                sourceh,
+                targetx,
+                targety,
+                targetw,
+                targeth,
+                buffer,
             });
             redraw();
             break;
@@ -537,6 +663,18 @@ window.addEventListener('DOMContentLoaded', () => {
     maxAttribCount = gl.getParameter(gl.MAX_VERTEX_ATTRIBS);
     canvas.width = windoww;
     canvas.height = windowh;
+
+    identityMatrix = new Float32Array([
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+    ]);
+
+    whitePixelTex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, whitePixelTex);
+    const texData = new Uint8Array([0xFF, 0xFF, 0xFF, 0xFF]);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, texData);
 
     // these values look to roughly match the ones used by the game, or near enough not to matter
     projMatrix = makeProjMatrix(gvw, gvh, 460, 65536 + 1024, 5 / 16);
